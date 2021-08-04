dc75774242e69ab23a19684b01e64cdd940462eb
edurso <eric@casadurso.com>
2021-08-04 15:25:43 -0400

    add nodejs to install script

--- install.sh
+++ install.sh
@@ -7,10 +7,11 @@
 sudo add-apt-repository ppa:lazygit-team/release
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/githubcli-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
 sudo apt-get update
 
 # Install dep packages
-declare -a packages=("git" "ripgrep" "neovim" "lazygit" "curl" "gh" "zsh")
+declare -a packages=("git" "ripgrep" "neovim" "lazygit" "curl" "gh" "zsh" "-y nodejs")
 for package in ${packages[@]}; do
     dpkg -s "$package" >/dev/null 2>&1 && {
         echo "$package is installed"
