75edfb58490039c4aeb486d4440eb0d498ff783c
edurso <eric@casadurso.com>
2021-08-15 23:27:56 -0400

    update install

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ set -o nounset
 set -o errexit
 
 # Set up apt-get repositories
-echo -ne '\n' | sudo add-apt-repository ppa:lazygit-team/release
+sudo add-apt-repository ppa:lazygit-team/release
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/githubcli-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
 curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
