5b9c747da0917bb3f32d5d4dc6c8f886bfc5334d
edurso <eric@casadurso.com>
2021-10-31 19:12:34 -0400

    add vsc to install

--- install.sh
+++ install.sh
@@ -1,25 +1,32 @@
 # Installs edurso's Dotfiles on Debian or Ubuntu
 # Author: @edurso
 
-# error when reference undefined vars
+# Error when reference undefined vars
 set -o nounset
 
-# exit when a command fails
-set -o errexit
+# Install libs for next step
+sudo apt install -y curl
+sudo apt install -y software-properties-common
+sudo apt install -y wget
+sudo apt install -y apt-transport-https
+sudo apt update
 
 # Set up apt-get repositories
 sudo add-apt-repository ppa:lazygit-team/release
+sudo add-apt-repository ppa:neovim-ppa/unstable
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/githubcli-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
 curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
-sudo apt-get update
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb https://packages.microsoft.com/repos/vscode stable main"
+sudo apt update
 
 # Install dep packages
 declare -a packages=(
     "git"
     "ripgrep"
+    "neovim"
     "lazygit"
-    "curl"
     "gh"
     "zsh"
     "nodejs"
@@ -34,14 +41,16 @@ declare -a packages=(
     "tree"
     "cmake"
     "gnupg2"
+    "code"
 )
 for package in ${packages[@]}; do
     dpkg -s "$package" >/dev/null 2>&1 && {
         echo "$package is installed"
     } || {
-        sudo apt-get install -y $package
+        sudo apt install -y $package
     }
 done
+sudo apt update
 
 # Make directory for git repository if it doesn't exist
 if [[ ! -d "$HOME/dotfiles" ]]; then
@@ -57,16 +66,82 @@ if [[ ! -e "$HOME/dotfiles/HEAD" ]]; then
     git --git-dir=$HOME/dotfiles/ --work-tree=$HOME reset --hard FETCH_HEAD
 fi
 
-# Download and Install NeoVim
-wget https://github.com/neovim/neovim/releases/download/v0.5.0/nvim.appimage
-chmod u+x ./nvim.appimage
-mv nvim.appimage $HOME/bin/
-
 # Set Up NeoVim (VundleVim plugins, etc.)
 if [[ ! -d "$HOME/.config/nvim/bundle/Vundle.vim" ]]; then
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.config/nvim/bundle/Vundle.vim
 fi
-$HOME/bin/nvim.appimage +PluginInstall +qall
+nvim +PluginInstall +qall
+
+# Install vs code extensions
+declare -a extensions=(
+    "austin.code-gnu-global"
+    "bierner.markdown-preview-github-styles"
+    "christian-kohler.path-intellisense"
+    "DavidAnson.vscode-markdownlint"
+    "dbankier.vscode-instant-markdown"
+    "docsmsft.docs-markdown"
+    "donjayamanne.githistory"
+    "dotdevru.prettier-java"
+    "ecmel.vscode-html-css"
+    "esbenp.prettier-vscode"
+    "fallenwood.vimL"
+    "GitHub.codespaces"
+    "GitHub.copilot"
+    "GitHub.github-vscode-theme"
+    "GitHub.vscode-pull-request-github"
+    "GrapeCity.gc-excelviewer"
+    "kisstkondoros.vscode-gutter-preview"
+    "kiteco.kite"
+    "luqimin.velocity"
+    "mdickin.markdown-shortcuts"
+    "ms-azuretools.vscode-docker"
+    "ms-dotnettools.csharp"
+    "ms-python.python"
+    "ms-python.vscode-pylance"
+    "ms-toolsai.jupyter"
+    "ms-toolsai.jupyter-keymap"
+    "ms-toolsai.jupyter-renderers"
+    "ms-vscode.cpptools"
+    "ms-vscode.powershell"
+    "ms-vscode.Theme-MarkdownKit"
+    "ms-vsliveshare.vsliveshare"
+    "ms-vsliveshare.vsliveshare-audio"
+    "ms-vsliveshare.vsliveshare-pack"
+    "mwpb.java-prettier-formatter"
+    "naco-siren.gradle-language"
+    "Pivotal.vscode-boot-dev-pack"
+    "Pivotal.vscode-concourse"
+    "Pivotal.vscode-manifest-yaml"
+    "Pivotal.vscode-spring-boot"
+    "PKief.material-icon-theme"
+    "rebornix.ruby"
+    "redhat.java"
+    "redhat.vscode-commons"
+    "redhat.vscode-xml"
+    "richardwillis.vscode-gradle-extension-pack"
+    "shd101wyy.markdown-preview-enhanced"
+    "tht13.html-preview-vscode"
+    "tomoki1207.pdf"
+    "torn4dom4n.latex-support"
+    "VisualStudioExptTeam.vscodeintellicode"
+    "vscjava.vscode-gradle"
+    "vscjava.vscode-java-debug"
+    "vscjava.vscode-java-dependency"
+    "vscjava.vscode-java-pack"
+    "vscjava.vscode-java-test"
+    "vscjava.vscode-maven"
+    "vscjava.vscode-spring-boot-dashboard"
+    "vscjava.vscode-spring-initializr"
+    "walkme.Markdown-extension-pack"
+    "wingrunr21.vscode-ruby"
+    "wpilibsuite.vscode-wpilib"
+    "yzane.markdown-pdf"
+    "yzhang.markdown-all-in-one"
+)
+for ext in ${extensions[@]}; do
+    code --install-extension $ext
+done
+
 
 # Install/update starship
 sh -c "$(curl -fsSL https://starship.rs/install.sh)"
