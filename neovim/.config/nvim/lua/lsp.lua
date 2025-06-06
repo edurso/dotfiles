1c981d4af544db8162a99b62448b33fe7b7ea770
Eric D'Urso <edurso@edurso.com>
2025-06-05 22:28:15 -0500

    add autosave nvim plugin

--- neovim/.config/nvim/lua/lsp.lua
+++ neovim/.config/nvim/lua/lsp.lua
@@ -23,19 +23,27 @@ lsp_zero.extend_lspconfig({
 require('mason').setup({})
 require('mason-lspconfig').setup({
     ensure_installed = {
-        -- full list of available ls: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
+        -- full list: github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md
         'rust_analyzer',
         'clangd',
         'lua_ls',
         'gradle_ls',
         'markdown_oxide',
-        -- 'nginx_language_server',
-        'pylsp',
+        'ruff',
         'svls',
     },
     handlers = {
         function(server_name)
             require('lspconfig')[server_name].setup({})
+        end,
+        ['ruff'] = function()
+            require('lspconfig').ruff.setup({
+                init_options = {
+                    settings = {
+                        lineLength = 120,
+                    },
+                },
+            })
         end,
     }
 })
