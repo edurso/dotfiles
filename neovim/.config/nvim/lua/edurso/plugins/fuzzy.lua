4bf2863cbbe1319eeae79244bf13d0c45374b5b0
Eric D'Urso <edurso@edurso.com>
2025-12-25 20:28:06 -0500

    bugfix nvim

--- neovim/.config/nvim/lua/edurso/plugins/fuzzy.lua
+++ neovim/.config/nvim/lua/edurso/plugins/fuzzy.lua
@@ -2,7 +2,7 @@
 return {
     "nvim-telescope/telescope.nvim",
     event = "VimEnter",
-    branch = "0.1.x",
+    branch = "master",
     dependencies = {
         "nvim-lua/plenary.nvim",
         { -- If encountering errors, see telescope-fzf-native README for installation instructions
